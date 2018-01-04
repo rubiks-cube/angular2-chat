7985580cb6f59057132886a4046644b007b2d0b6
Abhishek Ranjan <abhi03iiitm@gmail.com>
2018-01-04 23:03:15 +0530

    '2'

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { ChatService } from './services/chat.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule
+  ],
+  providers: [ChatService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client/src/app/components/chat/chat.component.ts
@@ -0,0 +1,88 @@
+import { Component, OnInit,HostListener } from '@angular/core';
+import { ChatService } from '../../services/chat.service';
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit {
+
+
+messages = [];
+  connectionMessage;
+  connectionUsers;
+  
+  message;
+  message1=null;
+  username;
+  load=false;
+  messageClass;
+  userInput;
+  users;
+check=[];
+status;
+
+
+
+  constructor(private chatService:ChatService) { }
+
+
+  @HostListener('window:beforeunload')
+  onExit() {
+    if (this.username) this.chatService.exitSession(this.username);
+  }
+
+  sendMessage() {
+    this.chatService.sendMessage(this.message, this.username);
+    this.message = '';
+    if (this.messages.length > 8) this.messages.splice(0, 1);
+  }
+
+  ngOnInit() {
+    this.connectionMessage = this.chatService.getMessages().subscribe(message => {
+
+      this.messages.push(message);
+    });
+
+
+    this.connectionUsers = this.chatService.getUsers().subscribe(data => {
+      this.users = data;
+       
+      this.check=this.users;
+    });
+    
+    
+    
+  }
+
+  saveUsername() {
+   
+   
+   console.log(this.check);
+  console.log( this.check.indexOf(this.userInput));
+   if(this.check.indexOf(this.userInput)>-1 ){
+console.log('lolol');
+   }
+   
+   else{
+   	this.chatService.saveUsername(this.userInput);
+   	 this.username=this.userInput;
+   }
+
+   
+  
+
+}
+  
+
+
+  
+
+
+
+
+    
+    
+
+
+}
